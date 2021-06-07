4fe40a1d10e4327fa7be91199c46678b8df3fc3f
GBstallman <nduwimanaguymich@gmail.com>
2021-06-07 03:28:27 -0700

    MainActivity changed to AuthActivity

---
+++ app/src/main/java/bi/gbstallman/pizzaphone/AuthActivity.java
@@ -0,0 +1,33 @@
+package bi.gbstallman.pizzaphone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import java.net.CookieHandler;
+import java.net.CookieManager;
+import java.net.CookiePolicy;
+
+
+public class AuthActivity extends AppCompatActivity {
+    private CookieManager cookieManager = null;
+    EditText username;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_auth);
+        username = findViewById(R.id.username);
+        cookieManager = new CookieManager();
+        cookieManager.setCookiePolicy(CookiePolicy.ACCEPT_ALL);
+        CookieHandler.setDefault(cookieManager);
+    }
+
+    public void pizzaliste(View view) {
+        Intent i = new Intent(this,MainActivity.class);
+        startActivity(i);
+    }
+}
